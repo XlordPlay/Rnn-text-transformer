8d3a18edc0fecd8ce79a57962c3cbfd1e9e42d34
XlordPlay <s.kirill150282@mail.ru>
2024-11-09 01:34:04 +0300

    add checkpoint.py

---
+++ training/checkpoint.py
@@ -0,0 +1,14 @@
+import torch 
+def save_checkpoint(model, optimizer, epoch, loss):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss,
+    }, 'checkpoint.pth')
+
+def load_checkpoint(model, optimizer):
+    checkpoint = torch.load('checkpoint.pth')
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    return checkpoint['epoch'], checkpoint['loss']
